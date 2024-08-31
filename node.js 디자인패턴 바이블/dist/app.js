028810438d6742dc019afe453b22036ca32ba600
imkdw <imkdw@kakao.com>
2024-08-31 20:09:24 +0900

    5-1 작업중

--- node.js 디자인패턴 바이블/dist/app.js
+++ node.js 디자인패턴 바이블/dist/app.js
@@ -1,36 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const events_1 = __importDefault(require("events"));
-let count = 0;
-const tick = (emitter, ms) => {
-    if (ms % 5 === 0) {
-        throw new Error("ms must be a multiple of 5");
-    }
-    count++;
-    emitter.emit("tick");
-};
-const ticker = (ms, cb) => {
-    const eventEmitter = new events_1.default();
-    try {
-        tick(eventEmitter, ms);
-        const interval = setInterval(() => tick(eventEmitter, ms), 50);
-        setTimeout(() => {
-            clearInterval(interval);
-            cb(count);
-        }, ms);
-    }
-    catch (err) {
-        eventEmitter.emit("error", err);
-    }
-    return eventEmitter;
-};
-const emitter = ticker(500, (count) => {
-    console.log(count);
-})
-    .on("tick", () => { })
-    .on("error", (err) => {
-    console.error(err);
+const promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.reject(new Error("error"))];
+Promise.allSettled(promises).then((results) => {
+    console.log(results);
 });
--- node.js 디자인패턴 바이블/src/app.ts
+++ node.js 디자인패턴 바이블/src/app.ts
@@ -1,51 +1,5 @@
-import EventEmitter from "events";
+const promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.reject(new Error("error"))];
 
-class TaskQueue extends EventEmitter {
-  private concurrency: number; // 동시성 제한
-  private queue: any[]; // 보류중인 작업 저장
-  private running: number; // 실행중인 모든 작업 개수
-  private completed: number;
-
-  constructor(concurrency: number) {
-    super();
-
-    this.concurrency = concurrency;
-    this.queue = [];
-    this.running = 0;
-    this.completed = 0;
-  }
-
-  pushTask(task: any) {
-    this.queue.push(task);
-    process.nextTick(this.next.bind(this));
-    return this;
-  }
-
-  next() {
-    while (this.running === 0 && this.queue.length === 0) {
-      return this.emit("empty");
-    }
-
-    while (this.running < this.concurrency && this.queue.length) {
-      const task = this.queue.shift();
-      task((err: unknown) => {
-        if (err) {
-          return this.emit("error", err);
-        }
-
-        this.running--;
-        process.nextTick(this.next.bind(this));
-      });
-      this.running++;
-    }
-  }
-}
-
-const queue = new TaskQueue(2);
-queue
-  .on("empty", () => {
-    console.log("all tasks are done");
-  })
-  .on("error", (err) => {
-    console.error(err);
-  });
+Promise.allSettled(promises).then((results) => {
+  console.log(results);
+});
