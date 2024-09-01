2ad9d9eb5e79b79ab2a6d881b76292a4ca5e2b9c
imkdw <imkdw@kakao.com>
2024-09-01 12:28:51 +0900

    5장 완료

--- node.js 디자인패턴 바이블/dist/app.js
+++ node.js 디자인패턴 바이블/dist/app.js
@@ -1,31 +1,24 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_1 = require("crypto");
-function promisify(callbackBasedApi) {
-    return function promisified(...args) {
-        // 새로운 Promise를 생성하고 반환함
-        return new Promise((resolve, reject) => {
-            // 콜백함수를 인자로 넘길때는 제일 마지막에 넘김
-            // 해당 특성을 이용해서 resolve, reject를 처리
-            const newArgs = [
-                ...args,
-                (err, result) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve(result);
-                },
-            ];
-            callbackBasedApi(...newArgs);
-        });
-    };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function delay(ms) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return setTimeout(() => { }, ms);
+    });
 }
-const randomBytesPromisified = promisify(crypto_1.randomBytes);
-randomBytesPromisified(10).then((bytes) => {
-    console.log(bytes);
-});
-function main() {
-    const randomBytesPromise = (0, crypto_1.randomBytes)(10);
-    console.log(randomBytesPromise);
+// @ts-ignore
+function leakingLoop() {
+    // @ts-ignore
+    return delay(1).then(() => {
+        console.log(`Tick ${Date.now()}`);
+        return leakingLoop();
+    });
 }
-main();
+leakingLoop();
--- node.js 디자인패턴 바이블/src/app.ts
+++ node.js 디자인패턴 바이블/src/app.ts
@@ -1,36 +1,14 @@
-import { randomBytes } from "crypto";
-
-function promisify(callbackBasedApi: any) {
-  return function promisified(...args: any[]) {
-    // 새로운 Promise를 생성하고 반환함
-    return new Promise((resolve, reject) => {
-      // 콜백함수를 인자로 넘길때는 제일 마지막에 넘김
-      // 해당 특성을 이용해서 resolve, reject를 처리
-      const newArgs = [
-        ...args,
-        (err: unknown, result: any) => {
-          if (err) {
-            return reject(err);
-          }
-
-          resolve(result);
-        },
-      ];
-
-      callbackBasedApi(...newArgs);
-    });
-  };
+async function delay(ms: number) {
+  return setTimeout(() => {}, ms);
 }
 
-const randomBytesPromisified = promisify(randomBytes);
-
-randomBytesPromisified(10).then((bytes) => {
-  console.log(bytes);
-});
-
-function main() {
-  const randomBytesPromise = randomBytes(10);
-  console.log(randomBytesPromise);
+// @ts-ignore
+function leakingLoop() {
+  // @ts-ignore
+  return delay(1).then(() => {
+    console.log(`Tick ${Date.now()}`);
+    return leakingLoop();
+  });
 }
 
-main();
+leakingLoop();
