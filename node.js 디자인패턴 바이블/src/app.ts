08a5fa6d7f414be492ef004fe9d7c08fb74c3b6e
imkdw <imkdw@kakao.com>
2024-09-07 23:44:29 +0900

    11-1. 비동기적으로 초기화되는 컴포넌트 다루기.md

--- node.js 디자인패턴 바이블/src/app.ts
+++ node.js 디자인패턴 바이블/src/app.ts
@@ -1,7 +1,71 @@
-function sayHello(name: string) {
-  if (typeof window === "undefined" && window.document) {
-    // client side codes
+import EventEmitter from "events";
+
+class Db extends EventEmitter {
+  private connected: boolean;
+  private commandsQueue: Array<() => void>;
+
+  constructor() {
+    super();
+    this.connected = false;
+    this.commandsQueue = [];
   }
 
-  // Node.js code
+  async query(queryString: string) {
+    if (!this.connected) {
+      console.log("Request Queued", queryString);
+
+      return new Promise((resolve, reject) => {
+        const command = () => {
+          this.query(queryString).then(resolve, reject);
+        };
+
+        this.commandsQueue.push(command);
+      });
+    }
+
+    console.log("Query executed", queryString);
+  }
+
+  connect() {
+    setTimeout(() => {
+      this.connected = true;
+      this.emit("connected");
+      this.commandsQueue.forEach((command) => command());
+      this.commandsQueue = [];
+    }, 500);
+  }
+}
+
+export const db = new Db();
+
+class InitializeState {
+  async query(queryString: string) {
+    console.log("query", queryString);
+  }
+}
+
+const METHODS_REQUIRING_CONNECTION = ["query"];
+const deactivate = Symbol("deactivate");
+class QueueingState {
+  private db: Db;
+  private commandsQueue: Array<() => void>;
+  private METHODS_REQUIRING_CONNECTION: string[];
+
+  constructor(db: Db) {
+    this.db = db;
+    this.commandsQueue = [];
+  }
+
+  METHODS_REQUIRING_CONNECTIOn.forEach((method) => {
+  (this as any)[method] = function (...args: any[]) {
+    console.log("Queueing", method, args);
+    return new Promise((resolve, reject) => {
+      const command = () => {
+        (this as any)[method](...args).then(resolve, reject);
+      };
+
+      this.commandsQueue.push(command);
+    });
+  };
+});
 }
