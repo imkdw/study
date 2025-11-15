6e0a81eb63b6dac324d344ea188c44e4d5a17c9f
dongwoo <imkdw@kakao.com>
2025-11-15 17:01:48 +0900

    1. 멀티패러다임이 현대 언어를 확장하는 방법 완료

--- temp.ts
+++ temp.ts
@@ -1,22 +1,39 @@
-const map = new Map([
-  ["a", 1],
-  ["b", 2],
-  ["c", 3],
-]);
-const mapEntries = map.entries();
-
-console.log(mapEntries.next()); // { value: ["a", 1], done: false }
-console.log(mapEntries.next()); // { value: ["b", 2], done: false }
-console.log(mapEntries.next()); // { value: ["c", 3], done: false }
-console.log(mapEntries.next()); // { value: undefined, done: true }
-
-const mapValues = map.values();
-for (const value of mapValues) {
-  console.log(value);
+function* map<A, B>(
+  func: (value: A) => B,
+  iterable: Iterable<A>
+): IterableIterator<B> {
+  for (const value of iterable) {
+    yield func(value);
+  }
 }
 
-const mapKeys = map.keys();
-for (const key of mapKeys) {
-  // a, b, c
-  console.log(key);
+function* naturals(end = Infinity) {
+  let n = 1;
+  while (n <= end) {
+    yield n++;
+  }
 }
+
+function* filter<T>(f: (value: T) => boolean, iterable: Iterable<T>) {
+  for (const value of iterable) {
+    if (f(value)) {
+      yield value;
+    }
+  }
+}
+
+function forEach(f: (value: unknown) => void, iterable: Iterable<unknown>) {
+  for (const value of iterable) {
+    f(value);
+  }
+}
+
+forEach(
+  console.log,
+  map(
+    (x) => x * 10,
+    filter((x) => x % 2 === 1, naturals(5))
+  )
+); // 10, 30, 50
+
+const nodes = document.querySelectorAll("li");
