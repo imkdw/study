4c011d293969887927e25be85c48f6514d533900
imkdw <imkdw@kakao.com>
2026-07-26 22:44:23 +0900

    npm

--- Node.js Deep Dive/app1.js
+++
@@ -1,8 +0,0 @@
-/**
- * Named Export : 중괄호 내부에서만 로딩 가능
- * Default Export : 괄호 불필요
- */
-import initializeLogger, { printMesage, version } from "./app";
-
-initializeLogger();
-printMesage(`ESM v${version}`);
---
+++ node_js_deep_dive/codes/app.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("BLUE 로그"));
+console.log(chalk.red.bold("RED 로그"));
+console.log(chalk.green.bgWhite("하얀 배경의 초록색 로그"));
